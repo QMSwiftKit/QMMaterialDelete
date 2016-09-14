af886fe7cbc3ed232ebfaed8466d35f3573a5f60
laznrbfe <laznrbfe@163.com>
2016-09-14 23:56:34 +0800

    init

---
+++ QMMaterialDeleteSwift/QMMaterialDeleteSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QMMaterialDeleteSwift
+//
+//  Created by 张青明 on 16/9/14.
+//  Copyright © 2016年 极客栈. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
